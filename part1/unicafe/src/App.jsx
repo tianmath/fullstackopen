b5cc636c6db14769615feb0d6cb4894fa9621ab1
tianmath <tiantsoamathieu@gmail.com>
2026-07-16 22:33:38 +0300

    do 1.6_unicafe_step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,43 @@
 import { useState } from 'react';
 
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+
+      <div>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <div>
+        <Button onClick={() => setGood(good + 1)} text='good' />
+        <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button onClick={() => setBad(bad + 1)} text='bad' />
+      </div>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
 };
 
 export default App;
