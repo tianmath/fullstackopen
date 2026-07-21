33276e63786b43fe2755e4ff5de465961be7bfef
tianmath <tiantsoamathieu@gmail.com>
2026-07-21 15:14:51 +0300

    do 2.1_courseinfo_step6 refactor by creating Course component

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,28 @@
+const Header = ({ course }) => <h1>{course.name}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
