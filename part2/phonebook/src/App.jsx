137dc43f85c509521a9243f1e354e10f0377250d
tianmath <tiantsoamathieu@gmail.com>
2026-07-28 23:02:20 +0300

    do 2.13_phonebook_step8 extract codes communicating with backend in own module

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import phonebookService from './services/phonebook';
 
 const Filter = ({ filter, onFilterChange }) => {
   return (
@@ -48,8 +48,8 @@ const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then((response) => {
-      setPersons(response.data);
+    phonebookService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -68,8 +68,8 @@ const App = () => {
       number: newNumber,
     };
 
-    axios.post('http://localhost:3001/persons', person).then((response) => {
-      setPersons(persons.concat(response.data));
+    phonebookService.create(person).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
       setNewName('');
       setNewNumber('');
     });
