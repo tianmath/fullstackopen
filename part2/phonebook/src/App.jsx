211488d79a7f6ed54d49fbfe45349d2909368507
tianmath <tiantsoamathieu@gmail.com>
2026-07-28 22:40:16 +0300

    do 2.12_phonebook_step7 save new numbers added to phonebook to backend

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -66,12 +66,13 @@ const App = () => {
     const person = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
     };
 
-    setPersons(persons.concat(person));
-    setNewName('');
-    setNewNumber('');
+    axios.post('http://localhost:3001/persons', person).then((response) => {
+      setPersons(persons.concat(response.data));
+      setNewName('');
+      setNewNumber('');
+    });
   };
 
   const handleNameChange = (e) => {
