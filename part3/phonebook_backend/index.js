7dc3b37c1e0d4808cfd6cf469c6e5d620c125c67
tianmath <tiantsoamathieu@gmail.com>
2026-08-11 15:25:05 +0300

    do 3.1_phonebook_backend_step1 return hardcoded list of phonebook entries

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -26,6 +26,10 @@ let persons = [
   },
 ];
 
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
