b8154839022c3378eb4a56d13a96c9064d5a321f
tianmath <tiantsoamathieu@gmail.com>
2026-08-13 19:40:43 +0300

    do 3.9_phonebook_backend_step9 connect phonebook app of part2 to phonebook_backend app of part3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 morgan.token('data', (request) => {
   return JSON.stringify(request.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data'),
