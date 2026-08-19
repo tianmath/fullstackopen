c5e800c42d0b0ebf77d12f5129925c978c7d4419
tianmath <tiantsoamathieu@gmail.com>
2026-08-19 15:22:56 +0300

    do 3.12_commandline_database create MongoDB Atlas based database and create a mongo.js file allowing us add entries to the database as well as list all of its entries

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('at least give password as argument');
+  process.exit(1);
+}
+
+if (process.argv.length > 5) {
+  console.log('too many arguments');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.se5we2g.mongodb.net/phonebookApp?appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const name = process.argv[3];
+const number = process.argv[4];
+
+if (name && number) {
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log('phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
