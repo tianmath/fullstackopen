e1a95b5bbb8a70d72eb2a969dcefcb41956ccf0b
tianmath <tiantsoamathieu@gmail.com>
2026-08-24 18:15:03 +0300

    do 3.19_phonebook_database_step7 use mongoose validation so name stored in database has to be at least three characters long

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -15,8 +15,12 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: { type: String, required: true },
 });
 
 personSchema.set('toJSON', {
