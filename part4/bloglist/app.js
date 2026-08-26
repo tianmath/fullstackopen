2dca088100110daca9e5c5d25fe2644a6bac5874
tianmath <tiantsoamathieu@gmail.com>
2026-08-26 13:25:47 +0300

    do 4.2_bloglist_step2 create custom logging module for printing app messages

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
+const logger = require('./utils/logger');
 const blogsRouter = require('./controllers/blogs');
 
 const app = express();
 
-console.log('connecting to', config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI);
+
 mongoose
   .connect(config.MONGODB_URI, { family: 4 })
   .then((result) => {
-    console.log('connected to MongoDB');
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
+    logger.error('error connecting to MongoDB:', error.message);
   });
 
 app.use(express.json());
