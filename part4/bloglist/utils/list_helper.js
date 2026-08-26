0d5a8a75123c499c0ad9610a44a87ace4e12478a
tianmath <tiantsoamathieu@gmail.com>
2026-08-26 21:05:51 +0300

    do 4.5_helper_functions_unit_test_step3 define favoriteBlog function and write tests for it

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  return blogs.reduce(
+    (withMostLikes, blog) =>
+      blog.likes > withMostLikes.likes ? blog : withMostLikes,
+    blogs[0],
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
