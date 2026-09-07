71af0f8acef314e261bfe4be8f439bf718d01bfd
tianmath <tiantsoamathieu@gmail.com>
2026-09-07 18:24:15 +0300

    4.10_bloglist_tests_step3 refactor endpoint responsible for creating a blog post to use async/await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,15 +6,11 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.status(201).json(savedBlog);
-    })
-    .catch((error) => next(error));
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
