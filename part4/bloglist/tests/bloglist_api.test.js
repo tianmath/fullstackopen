7fd992a56a2d53581bc88f3b8aecc967367a0560
tianmath <tiantsoamathieu@gmail.com>
2026-09-07 15:52:45 +0300

    4.8_bloglist_tests_step1 ensure all blogs are returned as JSON

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,15 +1,26 @@
-const { test, after } = require('node:test');
+const assert = require('node:assert');
+const { test, after, beforeEach } = require('node:test');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const helper = require('./test_helper');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-test('blogs are returned as json', async () => {
-  await api
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+test('all blogs are correctly returned in JSON format', async () => {
+  const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 after(async () => {
