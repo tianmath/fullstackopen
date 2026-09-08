60d3b41ca5c678a0445bd0a3fdf7c07d9d2a7041
tianmath <tiantsoamathieu@gmail.com>
2026-09-08 12:34:48 +0300

    4.13_bloglist_expansions_step1: write test for the endpoint responsible for deleting a single blog post resource

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert');
-const { test, after, beforeEach } = require('node:test');
+const { test, after, beforeEach, describe } = require('node:test');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
@@ -97,6 +97,22 @@ test('backend respondes with status code 400 if title or url is missing', async 
   };
 
   await api.post('/api/blogs').send(noURLBlog).expect(400);
+});
+
+describe('deletion of a blog post', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    const ids = blogsAtEnd.map((b) => b.id);
+    assert(!ids.includes(blogToDelete.id));
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+  });
 });
 
 after(async () => {
