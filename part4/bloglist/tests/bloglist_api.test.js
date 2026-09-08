cdf20244c46f1fc4db94fc14754c908a4cb50844
tianmath <tiantsoamathieu@gmail.com>
2026-09-08 18:19:54 +0300

    put tests related to creation of blog under the same describe block

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -29,74 +29,76 @@ test('the unique identifier property of a blog is named id', async () => {
   assert.strictEqual(someBlog._id.toString(), someBlog.toJSON().id);
 });
 
-test('a blog post can be created', async () => {
-  const newBlog = {
-    title: 'TDD harms architecture (instance 2)',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-  };
+describe('addition of a blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'TDD harms architecture (instance 2)',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+    };
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
 
-  const contents = blogsAtEnd.map((n) => n.title);
+    const contents = blogsAtEnd.map((n) => n.title);
 
-  assert(contents.includes('TDD harms architecture (instance 2)'));
-});
+    assert(contents.includes('TDD harms architecture (instance 2)'));
+  });
 
-test("likes default to 0 if missing from POST request's body", async () => {
-  const withLikesBlog = {
-    title: 'No likes blog',
-    author: 'Mr Peabody and Storm',
-    url: 'http://example.com/test-withLikes.html',
-    likes: 4,
-  };
+  test("defaults likes to 0 if likes is missing from POST request's body", async () => {
+    const withLikesBlog = {
+      title: 'No likes blog',
+      author: 'Mr Peabody and Storm',
+      url: 'http://example.com/test-withLikes.html',
+      likes: 4,
+    };
 
-  const withoutLikesBlog = {
-    title: 'No likes blog',
-    author: 'Mr Peabody and Storm',
-    url: 'http://example.com/test-withoutLikes.html',
-  };
+    const withoutLikesBlog = {
+      title: 'No likes blog',
+      author: 'Mr Peabody and Storm',
+      url: 'http://example.com/test-withoutLikes.html',
+    };
 
-  const responseWithLikes = await api
-    .post('/api/blogs')
-    .send(withLikesBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const responseWithLikes = await api
+      .post('/api/blogs')
+      .send(withLikesBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  const responseWithoutLikes = await api
-    .post('/api/blogs')
-    .send(withoutLikesBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const responseWithoutLikes = await api
+      .post('/api/blogs')
+      .send(withoutLikesBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-  assert.strictEqual(responseWithLikes.body.likes, 4);
-  assert.strictEqual(responseWithoutLikes.body.likes, 0);
-});
+    assert.strictEqual(responseWithLikes.body.likes, 4);
+    assert.strictEqual(responseWithoutLikes.body.likes, 0);
+  });
 
-test('backend respondes with status code 400 if title or url is missing', async () => {
-  const noTitleBlog = {
-    author: 'Mr Peabody and Storm',
-    url: 'http://example.com/test-noTitle.html',
-    likes: 0,
-  };
+  test('returns status code 400 if title or url is missing', async () => {
+    const noTitleBlog = {
+      author: 'Mr Peabody and Storm',
+      url: 'http://example.com/test-noTitle.html',
+      likes: 0,
+    };
 
-  await api.post('/api/blogs').send(noTitleBlog).expect(400);
+    await api.post('/api/blogs').send(noTitleBlog).expect(400);
 
-  const noURLBlog = {
-    title: 'No URL blog',
-    author: 'Mr Peabody and Storm',
-    likes: 0,
-  };
+    const noURLBlog = {
+      title: 'No URL blog',
+      author: 'Mr Peabody and Storm',
+      likes: 0,
+    };
 
-  await api.post('/api/blogs').send(noURLBlog).expect(400);
+    await api.post('/api/blogs').send(noURLBlog).expect(400);
+  });
 });
 
 describe('updating of a blog post', () => {
