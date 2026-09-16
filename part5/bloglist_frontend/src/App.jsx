9fed10802d55864cd6bcece1af93dc33e9cba761
tianmath <tiantsoamathieu@gmail.com>
2026-09-16 09:28:26 +0300

    5.2_bloglist_frontend_step2: implement logout

--- part5/bloglist_frontend/src/App.jsx
+++ part5/bloglist_frontend/src/App.jsx
@@ -35,6 +35,11 @@ const App = () => {
     }
   };
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedNoteappUser');
+    setUser(null);
+  };
+
   const loginForm = () => (
     <>
       <h2>log in to application</h2>
@@ -72,7 +77,9 @@ const App = () => {
       {user && (
         <>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p>
+            {user.name} logged in <button onClick={handleLogout}>logout</button>
+          </p>
 
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
