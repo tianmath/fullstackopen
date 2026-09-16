a3e520d7e60c1f55d36fb16771eb7c5de483c8c5
tianmath <tiantsoamathieu@gmail.com>
2026-09-16 08:39:35 +0300

    5.2_bloglist_frontend_step2: make login permanent using local storage

--- part5/bloglist_frontend/src/App.jsx
+++ part5/bloglist_frontend/src/App.jsx
@@ -13,11 +13,20 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
       setUser(user);
       setUsername('');
       setPassword('');
