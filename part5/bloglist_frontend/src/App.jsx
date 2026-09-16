6181069de1befda10d61757a45f2e7129351e1c0
tianmath <tiantsoamathieu@gmail.com>
2026-09-16 08:28:09 +0300

    5.1_bloglist_frontend_step1: implement login functionality to the frontend

--- part5/bloglist_frontend/src/App.jsx
+++ part5/bloglist_frontend/src/App.jsx
@@ -1,20 +1,75 @@
 import { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch {
+      console.log('wrong credentials');
+    }
+  };
+
+  const loginForm = () => (
+    <>
+      <h2>log in to application</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type='text'
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type='password'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </>
+  );
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {!user && loginForm()}
+
+      {user && (
+        <>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
