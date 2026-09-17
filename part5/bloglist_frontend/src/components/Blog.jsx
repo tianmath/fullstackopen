339b79039c6511f977fb50dbc48945e758b16603
tianmath <tiantsoamathieu@gmail.com>
2026-09-17 14:46:43 +0300

    5.7_bloglist_frontend_step7: for each blog add a button displaying/hiding its details

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} -- {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [displayDetails, setDisplayDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} -- {blog.author}{' '}
+        <button onClick={() => setDisplayDetails(!displayDetails)}>
+          {displayDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      {displayDetails && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user.username}</div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
