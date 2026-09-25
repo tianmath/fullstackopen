fa9c30ef757e3fa4264f12d15711b596088ffac1
tianmath <tiantsoamathieu@gmail.com>
2026-09-25 11:47:55 +0300

    5.19_bloglist_e2e_testing_step3: write test checking that a logged in user can create a blog

--- part5/bloglist_e2e_tests/tests/bloglist_app.spec.js
+++ part5/bloglist_e2e_tests/tests/bloglist_app.spec.js
@@ -1,5 +1,5 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test');
-const { loginWith } = require('./helper');
+const { loginWith, createBlog } = require('./helper');
 
 describe('Note app', () => {
   beforeEach(async ({ page, request }) => {
@@ -28,6 +28,24 @@ describe('Note app', () => {
     test('fails with wrong credentials', async ({ page }) => {
       await loginWith(page, 'chewara', 'wrong');
       await expect(page.getByText('Na logged in')).not.toBeVisible();
+    });
+  });
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'chewara', 'salainen');
+    });
+
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(
+        page,
+        'a blog created using playwright',
+        'playwright',
+        'http://example.com/blogage',
+      );
+      await expect(
+        page.getByText('a blog created using playwright').last(),
+      ).toBeVisible();
     });
   });
 });
--- part5/bloglist_e2e_tests/tests/helper.js
+++ part5/bloglist_e2e_tests/tests/helper.js
@@ -4,4 +4,15 @@ const loginWith = async (page, username, password) => {
   await page.getByRole('button', { name: 'login' }).click();
 };
 
-export { loginWith };
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'create new blog' }).click();
+
+  await page.getByLabel('title:').fill(title);
+  await page.getByLabel('author:').fill(author);
+  await page.getByLabel('url:').fill(url);
+
+  await page.getByRole('button', { name: 'create' }).click();
+  await page.getByText(title).last().waitFor();
+};
+
+export { loginWith, createBlog };
