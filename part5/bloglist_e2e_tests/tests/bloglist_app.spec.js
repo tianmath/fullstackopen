7ec64d8ea78d8510526343b2ba68d17ce9b11fc6
tianmath <tiantsoamathieu@gmail.com>
2026-09-25 10:57:43 +0300

    5.18_bloglist_e2e_testing_step2: write tests on successful and failed login

--- part5/bloglist_e2e_tests/tests/bloglist_app.spec.js
+++ part5/bloglist_e2e_tests/tests/bloglist_app.spec.js
@@ -1,11 +1,33 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test');
+const { loginWith } = require('./helper');
 
 describe('Note app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset');
+    await request.post('/api/users', {
+      data: {
+        name: 'Na',
+        username: 'chewara',
+        password: 'salainen',
+      },
+    });
+
     await page.goto('/');
   });
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('log in to application')).toBeVisible();
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'chewara', 'salainen');
+      await expect(page.getByText('Na logged in')).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'chewara', 'wrong');
+      await expect(page.getByText('Na logged in')).not.toBeVisible();
+    });
   });
 });
